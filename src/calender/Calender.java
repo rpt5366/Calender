559bcb768977a4b56bd6ddf002d3db1142d1d356
rpt5366 <43984143+rpt5366@users.noreply.github.com>
2019-03-07 12:59:23 +0900

    최대 일수 구하기 특정달의

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -1,5 +1,7 @@
 package calender;
 
+import java.util.Scanner;
+
 public class Calender {
 	public static void main(String[] args) {
 //		일 월 화 수 목 금 토
@@ -16,6 +18,15 @@ public class Calender {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("달을 입력: ");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d 일 까지 있습니다.\n", month, maxDays[month-1]);
+		scanner.close();
+		
 	}
 
 }
