1f1c1bae8221c42fbf0657a978183c98bead0807
rpt5366 <43984143+rpt5366@users.noreply.github.com>
2019-03-07 13:09:25 +0900

    Update using class and method

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -3,28 +3,33 @@ package calender;
 import java.util.Scanner;
 
 public class Calender {
-	public static void main(String[] args) {
-//		일 월 화 수 목 금 토
-//		--------------------
-//		 1  2  3  4  5  6  7
-//		 8  9 10 11 12 13 14
-//		15 16 17 18 19 20 21
-//		22 23 24 25 26 27 28
-
+	
+	private final int[] MAX_DAYS= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
+	public void printSampleCalender() {
 		System.out.println("일   월   화   수   목   금   토");
 		System.out.println("------------------------");
 		System.out.println("1   2   3   4   5   6  7");
 		System.out.println("8   9   10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+}
+	
+	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
+		// 객체 생성
+		Calender cal = new Calender();
 		System.out.print("달을 입력: ");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d월은 %d 일 까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		cal.printSampleCalender();
 		
-		System.out.printf("%d월은 %d 일 까지 있습니다.\n", month, maxDays[month-1]);
 		scanner.close();
 		
 	}
